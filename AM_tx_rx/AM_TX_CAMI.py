220b03fef79500110ac271a1187c884d4b1efe1f
MauricioOrozco <robert.mauricio.101295@gmail.com>
2024-02-12 10:46:36 -0500

    Modulacion AM: Se anade el documento con la descripcion

--- AM_tx_rx/AM_TX_CAMI.py
+++ AM_tx_rx/AM_TX_CAMI.py
@@ -68,7 +68,7 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.volume = volume = 1
-        self.samp_rate = samp_rate = 768000
+        self.samp_rate = samp_rate = 352800
         self.decim = decim = 16
 
         ##################################################
@@ -80,7 +80,7 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
         self.top_layout.addWidget(self._volume_win)
         self.qtgui_time_sink_x_1 = qtgui.time_sink_f(
             256, #size
-            48000, #samp_rate
+            samp_rate/decim, #samp_rate
             "", #name
             1, #number of inputs
             None # parent
@@ -95,11 +95,11 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_1.enable_autoscale(False)
         self.qtgui_time_sink_x_1.enable_grid(False)
         self.qtgui_time_sink_x_1.enable_axis_labels(True)
-        self.qtgui_time_sink_x_1.enable_control_panel(False)
+        self.qtgui_time_sink_x_1.enable_control_panel(True)
         self.qtgui_time_sink_x_1.enable_stem_plot(False)
 
 
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+        labels = ['Señal Demodulada', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
@@ -143,15 +143,15 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
         self.qtgui_time_sink_x_0.enable_autoscale(False)
         self.qtgui_time_sink_x_0.enable_grid(False)
         self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_control_panel(True)
         self.qtgui_time_sink_x_0.enable_stem_plot(False)
 
 
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+        labels = ['Señal Moduladora', 'Señal Modulada', 'Signal 3', 'Signal 4', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
+        colors = ['red', 'blue', 'green', 'black', 'cyan',
             'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
             1.0, 1.0, 1.0, 1.0, 1.0]
@@ -175,25 +175,26 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.freq_xlating_fir_filter_xxx_0 = filter.freq_xlating_fir_filter_fcf(16, firdes.low_pass(1,samp_rate,samp_rate/(2*decim), 2000), 48000, samp_rate)
+        self.constante = blocks.multiply_const_ff(volume)
         self.blocks_wavfile_source_0 = blocks.wavfile_source('/home/robert/Downloads/CantinaBand60 (mp3cut.net).wav', True)
         self.blocks_repeat_0 = blocks.repeat(gr.sizeof_float*1, 16)
         self.blocks_multiply_xx_0 = blocks.multiply_vff(1)
-        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(10)
+        self.blocks_multiply_const_vxx_1 = blocks.multiply_const_ff(volume)
         self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
         self.blocks_add_const_vxx_0 = blocks.add_const_ff(1)
         self.band_pass_filter_0 = filter.fir_filter_fff(
             1,
             firdes.band_pass(
                 1,
-                samp_rate,
+                48e3,
                 500,
                 6000,
                 400,
                 window.WIN_HAMMING,
                 6.76))
-        self.audio_sink_0 = audio.sink(samp_rate, '', True)
+        self.audio_sink_0 = audio.sink(22050, '', True)
         self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 48000, 0.2, 0, 0)
-        self.analog_agc_xx_0 = analog.agc_cc((1e-4), 1.0, 1.0, 65536)
+        self.analog_agc_xx_0 = analog.agc_cc((625e-6), 1.0, 1.0, 65536)
 
 
         ##################################################
@@ -201,16 +202,17 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
         ##################################################
         self.connect((self.analog_agc_xx_0, 0), (self.blocks_complex_to_mag_0, 0))
         self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.band_pass_filter_0, 0), (self.blocks_multiply_const_vxx_1, 0))
         self.connect((self.band_pass_filter_0, 0), (self.qtgui_time_sink_x_1, 0))
         self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_multiply_xx_0, 0))
         self.connect((self.blocks_complex_to_mag_0, 0), (self.band_pass_filter_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
-        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_1, 0), (self.audio_sink_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.freq_xlating_fir_filter_xxx_0, 0))
         self.connect((self.blocks_multiply_xx_0, 0), (self.qtgui_time_sink_x_0, 1))
-        self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.constante, 0))
         self.connect((self.blocks_wavfile_source_0, 0), (self.blocks_repeat_0, 0))
+        self.connect((self.constante, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.constante, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.freq_xlating_fir_filter_xxx_0, 0), (self.analog_agc_xx_0, 0))
 
 
@@ -227,6 +229,8 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
 
     def set_volume(self, volume):
         self.volume = volume
+        self.blocks_multiply_const_vxx_1.set_k(self.volume)
+        self.constante.set_k(self.volume)
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -234,9 +238,9 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
-        self.band_pass_filter_0.set_taps(firdes.band_pass(1, self.samp_rate, 500, 6000, 400, window.WIN_HAMMING, 6.76))
         self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(1,self.samp_rate,self.samp_rate/(2*self.decim), 2000))
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate/self.decim)
 
     def get_decim(self):
         return self.decim
@@ -244,6 +248,7 @@ class AM_TX_CAMI(gr.top_block, Qt.QWidget):
     def set_decim(self, decim):
         self.decim = decim
         self.freq_xlating_fir_filter_xxx_0.set_taps(firdes.low_pass(1,self.samp_rate,self.samp_rate/(2*self.decim), 2000))
+        self.qtgui_time_sink_x_1.set_samp_rate(self.samp_rate/self.decim)
 
 
 
